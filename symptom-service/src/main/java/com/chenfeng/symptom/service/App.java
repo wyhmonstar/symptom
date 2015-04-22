9489d6825fca51ca4ebba4cf86376eae6d5bff5a
wangyuhao <yuhao.wang@monstar-lab.com>
2015-04-22 22:36:18 +0800

    项目依赖关系

---
+++ symptom-service/src/main/java/com/chenfeng/symptom/service/App.java
@@ -0,0 +1,13 @@
+package com.chenfeng.symptom.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
